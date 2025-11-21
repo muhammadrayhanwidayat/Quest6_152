6b4697f2fb4575a022ec3352f97a4bdce210ad05
muhammadrayhanwidayat <murayat99@gmail.com>
2025-11-21 10:22:48 +0700

    membuat class view models

---
+++ app/src/main/java/com/example/project07/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.project07.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls: MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(nama = ls[0], gender = ls[1], alamat = ls[2])
+        }
+    }
+}
