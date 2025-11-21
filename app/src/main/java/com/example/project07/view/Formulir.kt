db7c59bf5a16a82f7e809f645ae2d5a464c64c51
muhammadrayhanwidayat <murayat99@gmail.com>
2025-11-21 10:36:57 +0700

    import dependensi

--- app/src/main/java/com/example/project07/view/Formulir.kt
+++ app/src/main/java/com/example/project07/view/Formulir.kt
@@ -26,6 +26,11 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.project07.R
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 
 
 @OptIn(ExperimentalMaterial3Api::class)
